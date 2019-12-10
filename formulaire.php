271ed27880d92021eba257b0cd3a357a02ed8057
alessdenys <alessandro.denys1999@gmail.com>
2019-12-10 10:48:06 +0100

    10/12 10H45

---
+++ formulaire.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>my todo list</title>
+</head>
+<body>
+    <h1>liste de choses à faire</h1>
+
+    <section id="inputSection">
+        <h2>ajout de nouvelles tâches</h2>
+        <input id="input" type="text"> 
+        <button id="ajout">ajouter</button>
+    </section>
+
+    <section id="todoSection">
+        <h2>tâches restantes</h2>
+    </section>
+</body>
+</html>
