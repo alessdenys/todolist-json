6f8657fda86be0c9697d27a98f188e76fd90dfa2
Vincent Bouton <vincent.bouton.dev@gmail.com>
2019-12-12 09:02:28 +0100

    Set-up Tailwindcss + gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,42 @@
-const gulp = require("gulp");
+const { watch, src, dest } = require("gulp");
+const tailwindConfig = "tailwind.config.js"; /* Path to Tailwind config */
+const mainCSS = "src/style.css"; /* Path to main stylesheet */
+const mainPHP = "index.php";
 
-class tailwindExtractor {
+/**
+ * Custom PurgeCSS Extractor
+ * https://github.com/FullHuman/purgecss
+ */
+
+class TailwindExtractor {
   static extract(content) {
-    return content.match(/[\w-/:]+(?<!:)/g);
+    return content.match(/[A-z0-9-:\/]+/g);
   }
 }
 
-gulp.task("css", () => {
+const css = () => {
+  const atimport = require("postcss-import");
   const postcss = require("gulp-postcss");
+  const tailwindcss = require("tailwindcss");
   const purgecss = require("gulp-purgecss");
-  return gulp
-    .src("Assets/css/src/style.css")
-    .pipe(postcss([require("tailwindcss"), require("autoprefixer")]))
+
+  return src(mainCSS)
+    .pipe(postcss([atimport(), tailwindcss(tailwindConfig)]))
     .pipe(
       purgecss({
-        content: ["index.php"],
+        content: ["**/*.php"],
         extractors: [
           {
-            extractor: tailwindExtractor,
-            extensions: ["php"]
+            extractor: TailwindExtractor,
+            extensions: ["css"]
           }
         ]
       })
     )
-    .pipe(gulp.dest("Assets/css/dist/"));
-});
+    .pipe(dest("dist/"));
+};
+
+exports.css = css;
+exports.default = () => {
+  watch(mainPHP, css);
+};
--- index.php
+++ index.php
@@ -4,10 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="./Assets/css/dist/style.css">
+    <link rel="stylesheet" href="dist/style.css">
     <title>Document</title>
+    
 </head>
-<body>
-    <h1 class="rounded">blabla</h1>
+<body class="container m-auto flex">
+    <div class="m-auto justify-center">
+        <h1 class="text-4xl center">To do List</h1>
+        <section id="inputSection">
+            <h2 class="">Add new task</h2>
+            <input class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-auto appearance-none leading-normal" id="input" type="text"> 
+            <button class="btn-blue" id="ajout">Add</button>
+        </section>
+        <section class="my-4" id="todoSection">
+            <h2>Task left : </h2>
+        </section>
+        <section class="my-4" id="doneSection">
+            <h2>Task done : </h2>
+        </section>
+    </div>
 </body>
 </html>
